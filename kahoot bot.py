36b663d298e141cf913bffb8b029e16daf754d7d
GamerBreadToaster <76438500+GamerBreadToaster@users.noreply.github.com>
2023-04-17 11:20:00 +0200

    Update kahoot bot.py

--- kahoot bot.py
+++ kahoot bot.py
@@ -5,7 +5,7 @@ bots = int(input("How many bots do you want to send: "))
 name = input("what name will the bot have: ")
 
 url = f"https://kahoot.it/?pin={code}&refer_method=link"
-chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe"
+chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe --start-maximized"
 webbrowser.get().open(url)
 time.sleep(1.5)
 kb.send("tab, tab")
@@ -13,11 +13,12 @@ kb.write(name)
 kb.send("enter")
 time.sleep(2)
 
-for i in range(bots - 1):
-    webbrowser.get().open(url)
-    time.sleep(0.2)
-    mouse.click('left')
-    time.sleep(1.1)
-    kb.send("tab, tab")
-    kb.write((name + str(i+2)))
-    kb.send("enter")
+if bots > 1:
+    for i in range(bots - 1):
+        webbrowser.get().open(url)
+        time.sleep(0.2)
+        mouse.click('left')
+        time.sleep(1.1)
+        kb.send("tab, tab")
+        kb.write((name + str(i+2)))
+        kb.send("enter")
